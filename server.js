48bd1fa0695320dc95f0b272a6d0f86fb86d67ec
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-06-08 22:38:03 -0500

    pushing new files

--- server.js
+++ server.js
@@ -44,18 +44,7 @@ app.delete("/api/notes/:id", function(req, res){
         if (err) {
         throw err;
     }
-    let object = JSON.parse(data);
-        for (note in object) {
-        if (object[note].id === target) {
-    object.splice(note, 1);
-    fs.writeFile("./db/db.json", `[${notes}]`, "utf-8", function(err){
-        if (err) throw err;
-        return;
-    });
-    }
-    };
-        res.end();
-    });
+    
 });
 
 
